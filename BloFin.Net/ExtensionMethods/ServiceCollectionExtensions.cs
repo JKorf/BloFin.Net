661cdf657f6b569cd80913c5d97e36122b736df7
Jkorf <jankorf91@gmail.com>
2026-09-15 10:04:03 +0200

    wip

--- BloFin.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ BloFin.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -1,18 +1,19 @@
-using CryptoExchange.Net;
-using CryptoExchange.Net.Clients;
-using CryptoExchange.Net.Interfaces;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Net.Http;
 using BloFin.Net;
 using BloFin.Net.Clients;
 using BloFin.Net.Interfaces;
 using BloFin.Net.Interfaces.Clients;
 using BloFin.Net.Objects.Options;
 using BloFin.Net.SymbolOrderBooks;
+using CryptoExchange.Net;
+using CryptoExchange.Net.Clients;
+using CryptoExchange.Net.Interfaces;
 using CryptoExchange.Net.Interfaces.Clients;
+using CryptoExchange.Net.SharedApis;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Net.Http;
 using System.Threading;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -121,17 +122,17 @@ namespace Microsoft.Extensions.DependencyInjection
                     x.GetRequiredService<IOptions<BloFinRestOptions>>(),
                     x.GetRequiredService<IOptions<BloFinSocketOptions>>()));
 
-            services.AddTransient<IBloFinSharedApiClient, BloFinSharedApiClient>();
-
-            services.RegisterSharedApi(x => x.GetRequiredService<IBloFinRestClient>().AccountApi.SharedApi);
-            services.RegisterSharedApi(x => x.GetRequiredService<IBloFinRestClient>().FuturesApi.SharedApi);
-            services.RegisterSharedApi(x => x.GetRequiredService<IBloFinSocketClient>().FuturesApi.SharedApi);
-
-            services.RegisterSharedApiClientCapabilities<IBloFinSharedApiClient>();
-
             services.RegisterSharedRestInterfaces(x => x.GetRequiredService<IBloFinRestClient>().AccountApi.SharedClient);
             services.RegisterSharedRestInterfaces(x => x.GetRequiredService<IBloFinRestClient>().FuturesApi.SharedClient);
             services.RegisterSharedSocketInterfaces(x => x.GetRequiredService<IBloFinSocketClient>().FuturesApi.SharedClient);
+
+            services.RegisterSharedApiClient<
+                IBloFinSharedApiClient,
+                BloFinSharedApiClient>(sharedApis => sharedApis
+                    .Add(client => client.AccountRest)
+                    .Add(client => client.FuturesRest)
+                    .Add(client => client.FuturesSocket)
+                    );
 
             return services;
         }
