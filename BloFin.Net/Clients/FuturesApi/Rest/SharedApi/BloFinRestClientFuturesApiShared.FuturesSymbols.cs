64884fa55591c5103682ab27cad03bc7e8eed528
Jkorf <jankorf91@gmail.com>
2026-09-22 15:37:52 +0200

    wip

--- BloFin.Net/Clients/FuturesApi/Rest/SharedApi/BloFinRestClientFuturesApiShared.FuturesSymbols.cs
+++ BloFin.Net/Clients/FuturesApi/Rest/SharedApi/BloFinRestClientFuturesApiShared.FuturesSymbols.cs
@@ -21,7 +21,7 @@ namespace BloFin.Net.Clients.FuturesApi
     {
         #region Get Futures Symbols
 
-        async Task<ICallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetFuturesSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public SharedSymbolCatalog? FuturesSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicId, _api.EnvironmentName, null);
